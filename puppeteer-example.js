e695077e9f1d6ed2b4d8b8b1ca364192f0848003
Jon Wicks <jmwicks@gmail.com>
2020-10-06 21:56:40 -0400

    Update puppeteer example
    Override existing content-disposition header if present

--- puppeteer-example.js
+++ puppeteer-example.js
@@ -51,10 +51,18 @@ const puppeteer = require('puppeteer');
       // console.log(reqEvent.responseHeaders);
 
       // Adding 'content-disposition: attachment' header will tell the browser to download the file instead of opening it in using built-in viewer
-      responseHeaders.push({
-        name: 'content-disposition',
-        value: 'attachment',
-      });
+      const foundHeaderIndex = responseHeaders.findIndex(
+        (h) => h.name === "content-disposition"
+      );
+      const attachmentHeader = {
+        name: "content-disposition",
+        value: "attachment",
+      };
+      if (foundHeaderIndex) {
+        responseHeaders[foundHeaderIndex] = attachmentHeader;
+      } else {
+        responseHeaders.push(attachmentHeader);
+      }
 
       /*
         Fetch.getResponseBody
